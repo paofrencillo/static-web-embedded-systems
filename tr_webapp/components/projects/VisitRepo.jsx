4a7bca465891dff8d02184be036b4299684f83ae
paopaolopaoloo <simonruzzel@gmail.com>
2022-11-21 16:16:13 +0800

    2nd commit

--- tr_webapp/components/projects/VisitRepo.jsx
+++ tr_webapp/components/projects/VisitRepo.jsx
@@ -10,13 +10,13 @@ const VisitRepo = ()=> {
             </h1>
             <div className={styles.github_container}>
                 <div className={styles.github_links_wrapper}>
-                    <Link className={styles.github_links} href={'https://github.com/paopaolopaoloo/THESIS-WEB-APP'}>
+                    <Link className={styles.github_links} href={'https://github.com/paopaolopaoloo/THESIS-WEB-APP'} target={'_blank'}>
                         <img className={styles.github_img} src="/images/github.png" alt="github" />
                         <h4 className={styles.github_repo}>Tool Keeper System</h4>
                     </Link>
                 </div>
                 <div className={styles.github_links_wrapper}>
-                    <Link className={styles.github_links} href={'https://github.com/paopaolopaoloo/THESIS-WEB-APP'}>
+                    <Link className={styles.github_links} href={'https://github.com/paopaolopaoloo/static_web'} target={'_blank'}>
                         <img className={styles.github_img} src="/images/github.png" alt="github" />
                         <h4 className={styles.github_repo}>Static Website using next.js</h4>     
                     </Link>
